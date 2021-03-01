09e46385608336ed20b8a749e481b18069810c08
Damian Goraj <damiangoraj@gmail.com>
2021-03-01 08:27:43 +0100

    Initial commit

---
+++ src/test/java/SpringInAction/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringInAction.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
